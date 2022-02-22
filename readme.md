1d1b1dce24cd0144cdee8e549ae31962eda90e6e
Pedro Acordi <pedroacordi2@gmail.com>
2022-02-22 18:27:11 -0300

    Pré-Finalização

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-Esse projeto é destinado ao aprendizado. É uma aplicação que visa notificar o tempo em uma determinada cidade.
+# App para verificação do tempo
+## Weather app
+
+Aplicação destinada à verificação do tempo em um determinado local. Feito apenas para aprendizado.
+*Web application for checking the weather of a certain place. Made only for learning.*
+
+Ainda não funciona em português, devido à dificuldades com APIs de tradução. Por enquanto, apenas em inglês.
+
+
+
+Pedro.
--- script.js
+++ script.js
@@ -1,14 +1,55 @@
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 const apikey = '4f0c5f237b428cfe861db9dc06e5875a';
 
-const url = (location, key)=> `https://api.openweathermap.org/data/2.5/weather?q=
-${location}&appid=${apikey}`;
+const url = (city, key)=> `https://api.openweathermap.org/data/2.5/weather?q=
+${city}&appid=${apikey}`;
 
-async function getWeatherByLocation(location){
-    const resp = await fetch(url(location),{
+async function getWeatherBycity(city){
+    const resp = await fetch(url(city),{
     origin: "cors"});
     const respData = await resp.json();
 
+    addWeatherToPage(respData);
     console.log(respData);
 }
 
-getWeatherByLocation('Cascavel');
+function addWeatherToPage(data){
+    const temp=KtoC(data.main.temp);
+    const tempMax = KtoC(data.main.temp_max);
+    const tempMin = KtoC(data.main.temp_min);
+    const weather = document.createElement("div");
+    weather.classList.add('weather');
+    const titleCase = text=>{
+        return text.toLowerCase().split(' ').map((word)=>{
+            return word[0].toUpperCase() + word.slice(1);
+        }).join(' ')
+    }
+
+    weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>${temp}°C</h2>
+        <small>Max: ${tempMax}°C Min: ${tempMin}°C</small>
+        <small class="trn" id="text-to-translate">${data.weather[0].main}<small>
+        <p>in ${titleCase(search.value)}</p>
+    `;
+
+    //Clean up
+    main.innerHTML="";
+
+    main.appendChild(weather);
+}
+
+function KtoC(K){
+    return Math.floor((K - 273.15));
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const city = search.value;
+    if(city){
+        getWeatherBycity(city);
+    }
+});
